5e94fc5ed656f38dd259611c497b543b4feed63f
islekcaganmert <islekcaganmert@hereus.net>
2025-03-29 12:18:14 +0300

    Protected Mode & Code Cleanup

--- compile.sh
+++ compile.sh
@@ -5,17 +5,26 @@ if [ -d "build" ]; then
   rm -rf build
 fi
 mkdir dist build build/{kernel,iso}
-i686-elf-as kernel/boot.asm -o build/kernel/boot.o || exit 1
-i686-elf-gcc -c kernel/kernel.c -o build/kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra || exit 1
-i686-elf-gcc -T kernel/linker.ld -o build/kernel/kernel.bin -ffreestanding -O2 -nostdlib build/kernel/boot.o build/kernel/kernel.o -lgcc || exit 1
 
-if grub-file --is-x86-multiboot build/kernel/kernel.bin; then
+# Compile assembly
+i686-elf-as kernel/boot.asm -o build/kernel/boot.o || exit 1
+
+# Compile C files
+i686-elf-gcc -c kernel/kernel.c -o build/kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra || exit 1
+i686-elf-gcc -c kernel/terminal.c -o build/kernel/terminal.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra || exit 1
+i686-elf-gcc -c kernel/string.c -o build/kernel/string.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra || exit 1
+
+# Link all object files
+i686-elf-gcc -T kernel/linker.ld -o build/kernel/kernel.bin -ffreestanding -O2 -nostdlib build/kernel/*.o -lgcc || exit 1
+
+if i686-elf-grub-file --is-x86-multiboot build/kernel/kernel.bin; then
   echo multiboot confirmed
 else
   echo the file is not multiboot
+  exit 1
 fi
 
-mkdir build/iso/boot
+mkdir build/iso/{boot,bin,etc,lib,usr}
 mkdir build/iso/boot/grub
 cp build/kernel/kernel.bin build/iso/boot/kernel.bin
 cp bootloader/grub.cfg build/iso/boot/grub/grub.cfg
--- kernel/boot.asm
+++ kernel/boot.asm
@@ -10,6 +10,30 @@
 .long FLAGS
 .long CHECKSUM
 
+.section .data
+.align 16
+gdt_start:
+    .quad 0
+gdt_code:
+    .word 0xFFFF
+    .word 0x0000
+    .byte 0x00
+    .byte 0x9A
+    .byte 0xCF
+    .byte 0x00
+gdt_data:
+    .word 0xFFFF
+    .word 0x0000
+    .byte 0x00
+    .byte 0x92
+    .byte 0xCF
+    .byte 0x00
+gdt_end:
+
+gdt_descriptor:
+    .word gdt_end - gdt_start - 1
+    .long gdt_start
+
 .section .bss
 .align 16
 stack_bottom:
@@ -22,8 +46,22 @@ stack_top:
 
 _start:
     mov $stack_top, %esp
-    call kernel_main
     cli
+    lgdt gdt_descriptor
+    mov %cr0, %eax
+    or $0x1, %eax
+    mov %eax, %cr0
+    ljmp $0x08, $protected_mode_entry
+
+.code32
+protected_mode_entry:
+    mov $0x10, %ax
+    mov %ax, %ds
+    mov %ax, %es
+    mov %ax, %fs
+    mov %ax, %gs
+    mov %ax, %ss
+    call kernel_main
 1:  hlt
     jmp 1b
 
